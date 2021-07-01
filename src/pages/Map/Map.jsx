3f3b1ec1c41d0bb610aabcb3bad6aea5b535e350
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-01 11:09:43 +0200

    BUG: api request in Map component, not working

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { MapImage, Navbar } from '../../components';
+import React, { useEffect } from 'react';
+import { MapImage } from '../../components';
 
 const Map = () => {
+  const BASE_URL = 'https://creek-finder.herokuapp.com';
+
+  console.log('Entra en Map');
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/creeks`)
+      .then((res) => res.json())
+      .then((dataRes) => {
+        let creeks = dataRes;
+
+        console.log(creeks);
+      });
+  }, []);
+
   return (
     <main>
-      <MapImage />
+      <MapImage creeks={''} />
     </main>
   );
 };
