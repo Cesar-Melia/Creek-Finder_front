0f444458b640f164621b55169ea96dee6dbe82ee
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-02 18:48:46 +0200

    FEAT: photo galley implemented on detail component

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
 import { BackButton } from '../../components';
+// import Carousel from 'react-bootstrap/Carousel';
+import ReactBnbGallery from 'react-bnb-gallery';
+import 'react-bnb-gallery/dist/style.css';
 // import { withRouter } from 'react-router-dom';
 
 const Detail = (props) => {
-  return (
-    <main>
-      <BackButton />
-      Detail working
-    </main>
-  );
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (props.creek) {
+    const PHOTOS = props.creek.img.map((img) => img);
+    return (
+      <main>
+        <BackButton />
+        <button onClick={() => setIsOpen(true)}>Open gallery</button>
+        <ReactBnbGallery show={isOpen} photos={PHOTOS} onClose={() => setIsOpen(false)} />
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <p>cargando...</p>
+      </main>
+    );
+  }
 };
 
 export default Detail;
