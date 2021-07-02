57d64a7f7731764a3b1e1c54c8d4531f460fde55
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-02 09:46:35 +0200

    STYLE: creeks card transparency

--- src/pages/Creeks/Creeks.jsx
+++ src/pages/Creeks/Creeks.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import CreekCard from '../../components/CreekCard/CreekCard'
+import CreekCard from '../../components/CreekCard/CreekCard';
 
 import './Creek.scss';
 
 const Creeks = ({ creeks }) => {
-
   return (
-    <div className="list">
-      <div className="list__content">
+    <div className='list'>
+      <div className='list__content'>
         <h2>Creek List</h2>
       </div>
-      <div >
-        <div className="container">
-          <div className="row">
-            {creeks.map(creek => {
-              return (<CreekCard key={JSON.stringify(creek)} creek={creek} />)
+      <div>
+        <div className='container'>
+          <div className='row'>
+            {creeks.map((creek) => {
+              return <CreekCard key={JSON.stringify(creek)} creek={creek} />;
             })}
           </div>
-
         </div>
       </div>
-
-    </div >
-
+    </div>
   );
 };
 
