126d5cb5978d5499d2d43f68a79020fb2a20e67d
frank <frankestrada41@hotmail.com>
2021-07-04 13:59:42 +0200

    FEAT:styles register

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 
 import './RegisterForm.scss'
 
+const BASE_URL = 'http://localhost:3500';
+
 const RegisterForm = () => {
+
+  const submitForm = async (ev) => {
+    ev.preventDefault()
+    const { userName, email, password } = ev.target
+    const requestOptions = { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ userName: userName.value, email: email.value, password: password.value }) }
+    const us = await fetch(`${BASE_URL}/auth/register`, requestOptions)
+    const usData = await us.json()
+    console.log('respuesta fetch REGISTRO', usData)
+  }
+
   return (
     <div className="register-container">
       <div className="register-container__main">
         <h2>Creek Finder</h2>
-        <form className="register-container__form">
+        <form onSubmit={submitForm} className="register-container__form">
           <label>
             <p>Nombre de usuario</p>
             <input type="text" name="userName" />
