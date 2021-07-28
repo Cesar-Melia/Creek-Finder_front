56d4f81037a891c5aff1709633421658db48ba89
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-29 01:17:43 +0200

    FEAT: footer elements

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,74 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import World from '../../assets/world.png'
-import './Footer.scss'
+import World from '../../assets/world2.png';
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <div>
-        <img src={World} alt="" className="footer-container__img" />
+    <div className='footer'>
+      <div className='footer__menu'>
+        <img
+          src='https://res.cloudinary.com/creek-finder/image/upload/v1627511412/backgrounds/footer_wave_rqy0ew.png'
+          alt='wave'
+          className='footer__wave'
+        />
+        <div>
+          <ul>
+            <h2 className='footer__title'>Explora Calas aaaa</h2>
+            <li>
+              <Link to='/map' className='footer__link'>
+                Mapa de España
+              </Link>
+            </li>
+            <li>
+              <Link to='/top-creeks' className='footer__link'>
+                Top 10
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <h2 className='footer__title'>Creek Finder</h2>
+            <li>
+              <Link to='/about' className='footer__link'>
+                Sobre Nosotros
+              </Link>
+            </li>
+            <li>
+              <Link to='/creators' className='footer__link'>
+                Creadores
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <img src={World} alt='nosotros' className='footer__img' />
+        <div>
+          <ul>
+            <h2 className='footer__title'>Ayuda</h2>
+            <li>
+              <Link exact to='/contact' className='footer__link'>
+                Contacto
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <h2 className='footer__title'>Servicios</h2>
+            <li>
+              <Link to='/promo' className='footer__link'>
+                Promociona tu negocio
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div>
-        <Link exact to="/about" className="footer-container__link">Sobre Nosotros</Link>
+        <span className='footer__copyright'>© CreekFinder. All rights reserved.</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
