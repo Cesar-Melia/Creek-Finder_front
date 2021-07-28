c1109fe55a65f1690e642d1fe8ccffca89db2e76
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-28 18:00:13 +0200

    STYLE: home carousel

--- src/components/CarouselComp/CarouselComp.jsx
+++ src/components/CarouselComp/CarouselComp.jsx
@@ -11,12 +11,13 @@ const CarouselComp = ({ creeks }) => {
 
   return (
     <Carousel activeIndex={index} onSelect={handleSelect} touch='true' className='carousel'>
-      {creeks.map((creek) => {
+      {creeks.map(creek => {
         return (
           <Carousel.Item interval={5000} key={JSON.stringify(creek)}>
             <img className='carousel__img' src={creek.img[0]} alt={creek.name} />
             <Carousel.Caption>
-              <h3>{creek.name}</h3>
+              <h3 className='carousel__title'>{creek.name}</h3>
+              <h3 className='carousel__subtitle'>{creek.province}</h3>
             </Carousel.Caption>
           </Carousel.Item>
         );
